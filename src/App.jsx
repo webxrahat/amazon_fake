7a31044535190401d0ca215f47dfa4ec8e129b5e
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-30 21:29:16 +0600

    hg

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import RoutesLayout from "./routes/RoutesLayout";
+
+// https://dummyjson.com/products
+
+export default function App() {
+ return (
+  <>
+   <div>
+    <RoutesLayout />
+   </div>
+  </>
+ );
+}
