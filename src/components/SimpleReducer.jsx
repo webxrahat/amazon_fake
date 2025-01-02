e1e9f41881704e88d3ea048bfdb2b78631606bbb
Md Rahat Islam <islammdrahat308@gmail.com>
2025-01-02 22:40:08 +0600

    gh

---
+++ src/components/SimpleReducer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SimpleReducer = () => {
+ return <div>SimpleReducer</div>;
+};
+
+export default SimpleReducer;
--- src/routes/RoutesLayout.jsx
+++ src/routes/RoutesLayout.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import Home from "../pages/Home";
 import SingleProduct from "../pages/SingleProduct";
+import SimpleReducer from "../components/SimpleReducer";
 
 const RoutesLayout = () => {
  return (
   <>
    <Routes>
-    <Route path="/" element={<Home />} />
+    <Route path="/" element={<SimpleReducer />} />
     <Route path="/product/:id" element={<SingleProduct />} />
    </Routes>
   </>
