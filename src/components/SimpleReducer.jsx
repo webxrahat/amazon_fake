a3299850d95c60da480e7cf56fe022b08c81aaba
Md Rahat Islam <islammdrahat308@gmail.com>
2025-01-03 00:38:13 +0600

    gh

--- src/components/SimpleReducer.jsx
+++ src/components/SimpleReducer.jsx
@@ -2,13 +2,47 @@ import React, { useReducer, useState } from "react";
 
 const SimpleReducer = () => {
  const [counter, setCounter] = useState(0);
- // const [state, dispatch] = useReducer()
+ const [state, dispatch] = useReducer(reducer, 0);
+
+ //  console.log(dispatch);
+
+ function reducer(state, action) {
+  if (action.type === "increment") {
+   return state + action.payload;
+  }
+ }
+
+ const handleDecrement = () => {
+  counter === 0 ? counter : setCounter(counter - 1);
+ };
 
  return (
   <div>
-   {counter}
-   <button onClick={() => setCounter(counter + 1)}>Increment</button>
-   <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+   use-state: {counter}
+   <br />
+   use-reducer: {state}
+   <br />
+   <button
+    className="bg-green-200 p-2 mr-5"
+    onClick={() => setCounter(counter + 1)}
+   >
+    Increment
+   </button>
+   <button className="bg-red-200 p-2" onClick={() => handleDecrement()}>
+    Decrement
+   </button>
+   <br />
+   <div className="mt-2">
+    <button
+     className="bg-green-200 p-2 mr-5"
+     onClick={() => dispatch({ type: "increment", payload: 1 })}
+    >
+     Increment Count
+    </button>
+    <button className="bg-red-200 p-2" onClick={() => dispatch}>
+     Decrement Count
+    </button>
+   </div>
   </div>
  );
 };
